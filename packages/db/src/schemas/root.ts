c597f4cc1489a5b094fc3c39a6e055ff24b3a8d3
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-22 17:46:24 -0500

    FEAT: export all db schemas ⚒️

---
+++ packages/db/src/schemas/root.ts
@@ -0,0 +1,8 @@
+export {
+  combatStylesRelations,
+  resonatorRelations,
+  combatStyles,
+  resonators,
+} from "../schemas/resonator-schema";
+export { account, session, user, verification } from "../schemas/auth-schema";
+export { weapons } from "../schemas/weapon-schema";
