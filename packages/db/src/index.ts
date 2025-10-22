f2f5e5ba9782ce4bccd87effd064f278cdf27506
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-22 17:45:17 -0500

    FEAT: add db package ⚙️

---
+++ packages/db/src/index.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../apps/web/.env" });
+
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+import * as schema from "./schemas/root";
+
+const client = createClient({
+  url: process.env.DATABASE_URL || "",
+  authToken: process.env.DATABASE_AUTH_TOKEN,
+});
+
+export const db = drizzle({ client, schema });
