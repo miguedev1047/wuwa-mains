cce7876c23f7a033f3b00d93fb5753bc733769b2
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-22 17:45:48 -0500

    FEAT: add db package ⚙️

---
+++ packages/db/drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../apps/web/.env" });
+
+export default defineConfig({
+  schema: "./src/schemas",
+  out: "./src/migrations",
+  dialect: "turso",
+  dbCredentials: {
+    url: process.env.DATABASE_URL || "",
+    authToken: process.env.DATABASE_AUTH_TOKEN,
+  },
+});
