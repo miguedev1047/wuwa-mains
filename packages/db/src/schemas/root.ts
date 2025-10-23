84ecf833da90c124661c8e4ab6fa9ab0143bf38b
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-23 07:26:28 -0500

    CHORE: rename schema db folder ⚒️

--- packages/db/src/schemas/root.ts
+++
@@ -1,8 +0,0 @@
-export {
-  combatStylesRelations,
-  resonatorRelations,
-  combatStyles,
-  resonators,
-} from "../schemas/resonator-schema";
-export { account, session, user, verification } from "../schemas/auth-schema";
-export { weapons } from "../schemas/weapon-schema";
