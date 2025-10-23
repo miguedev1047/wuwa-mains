9ac71267b776e23564010212228bdfc855778e1b
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-23 10:46:56 -0500

    CHORE: minor changes ⚙️

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -13,7 +13,7 @@ export const appRouter = router({
     };
   }),
   resonators: resonatorsRouter,
-  weapon: weaponsRouter,
+  weapons: weaponsRouter,
 });
 
 export type AppRouter = typeof appRouter;
