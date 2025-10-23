a5a7f294809cec567979663653810a18118de8d3
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-23 07:32:42 -0500

    FEAT: add find route hook ⚙️

---
+++ apps/web/src/hooks/use-find-route.ts
@@ -0,0 +1,14 @@
+import { PANEL_ROUTES } from '@/constants/panel-routes'
+import { useLocation } from '@tanstack/react-router'
+
+export function useFindRoute() {
+  const location = useLocation()
+  const pathname = location.pathname
+
+  const filteredRoutes = PANEL_ROUTES.navMain.find((route) =>
+    route.items.find((item) => pathname.includes(item.url))
+  )
+  const findRoute = filteredRoutes?.items.find((item) => pathname.includes(item.url))
+
+  return { findRoute }
+}
