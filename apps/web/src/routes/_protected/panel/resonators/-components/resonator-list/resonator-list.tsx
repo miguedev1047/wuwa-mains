a03947492522258fc8bdaa123e8a2b4cf1be0368
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-23 10:32:11 -0500

    FEAT: add resonator list component 🎨

---
+++ apps/web/src/routes/_protected/panel/resonators/-components/resonator-list/resonator-list.tsx
@@ -0,0 +1,26 @@
+import {
+  ResonatorEmpty,
+  ResonatorItem,
+} from "@/routes/_protected/panel/resonators/-components";
+import { cn } from "@/lib/utils";
+import { listGridClass } from "@/tw-class";
+import { useResonatorList } from "@/routes/_protected/panel/resonators/-hooks";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+export function ResonatorList() {
+  const { filteredResonators } = useResonatorList();
+
+  if (!filteredResonators.length) return <ResonatorEmpty />;
+
+  const MAPPED_RESONATORS = filteredResonators.map((resonator) => (
+    <li key={resonator.id}>
+      <ResonatorItem {...resonator} />
+    </li>
+  ));
+
+  return (
+    <TooltipProvider>
+      <ul className={cn(listGridClass)}>{MAPPED_RESONATORS}</ul>
+    </TooltipProvider>
+  );
+}
