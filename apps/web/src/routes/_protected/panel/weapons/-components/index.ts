a41960ae73a6a455468762bc6287eb7eb99f8ce8
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-23 10:49:45 -0500

    FEAT: export all weapon components ⚒️

---
+++ apps/web/src/routes/_protected/panel/weapons/-components/index.ts
@@ -0,0 +1,6 @@
+export * from "./weapon-header";
+export * from "./weapon-empty";
+export * from "./weapon-list";
+export * from "./weapon-item";
+export * from "./weapon-actions";
+export * from "./weapon-form";
