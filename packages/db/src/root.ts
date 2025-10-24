9ae01beab7547e7d101be8b024c3ee0f40169011
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-24 11:17:09 -0500

    FEAT: add echo db schema ⚒️

--- packages/db/src/root.ts
+++ packages/db/src/root.ts
@@ -4,6 +4,12 @@ export {
   combatStyles,
   resonators,
 } from "./schema/resonator-schema";
+export {
+  echoesSetRelations,
+  echoesRelations,
+  echoesSet,
+  echoes,
+} from "./schema/echo-schema";
 export { account, session, user, verification } from "./schema/auth";
 export { weapons } from "./schema/weapon-schema";
 export { materials } from "./schema/material-schema";
