8472ae396e46c6c13dd34835c1c3be8135deffaa
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-24 11:22:32 -0500

    FEAT: add panel echoes route ⚒️

---
+++ apps/web/src/routes/_protected/panel/echoes/index.tsx
@@ -0,0 +1,34 @@
+import {
+  EchoHeader,
+  EchoList,
+} from "@/routes/_protected/panel/echoes/-components";
+import { ErrorState } from "@/components/state-ui/error";
+import { LoaderState } from "@/components/state-ui/loader";
+import { createFileRoute } from "@tanstack/react-router";
+import { createStandardSchemaV1 } from "nuqs";
+import { searchParams } from "@/routes/_protected/panel/echoes/-hooks";
+import { ListSkeleton } from "@/components/state-ui/skeletons";
+import { Suspense } from "react";
+
+export const Route = createFileRoute("/_protected/panel/echoes/")({
+  pendingComponent: () => <LoaderState title="Cargando ecos..." />,
+  errorComponent: () => <ErrorState title="Error al cargar los ecos." />,
+  loader: async ({ context }) => {
+    await context.queryClient.ensureQueryData(
+      context.trpc.echoes.get.queryOptions(),
+    );
+  },
+  validateSearch: createStandardSchemaV1(searchParams, { partialOutput: true }),
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <main className="space-y-6 w-full min-h-[calc(100vh-10rem)]">
+      <EchoHeader />
+      <Suspense fallback={<ListSkeleton />}>
+        <EchoList />
+      </Suspense>
+    </main>
+  );
+}
