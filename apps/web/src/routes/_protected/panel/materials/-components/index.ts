bfe893cd59c24cfce0dc51db95da5e7794155804
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-23 19:03:11 -0500

    FEAT: export all materials components ⚒️

---
+++ apps/web/src/routes/_protected/panel/materials/-components/index.ts
@@ -0,0 +1,6 @@
+export * from "./material-header";
+export * from "./material-empty";
+export * from "./material-list";
+export * from "./material-item";
+export * from "./material-actions";
+export * from "./material-form";
