844299d86ab6666125653465d4ae5676d451e17c
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-23 19:02:06 -0500

    FEAT: add panel weapon route ⚒️

--- apps/web/src/routes/_protected/panel/weapons/$id/index.tsx
+++ apps/web/src/routes/_protected/panel/weapons/$id/index.tsx
@@ -5,6 +5,7 @@ import {
 import { ErrorState } from "@/components/state-ui/error";
 import { LoaderState } from "@/components/state-ui/loader";
 import { createFileRoute } from "@tanstack/react-router";
+
 export const Route = createFileRoute("/_protected/panel/weapons/$id/")({
   pendingComponent: () => <LoaderState title="Cargando arma..." />,
   errorComponent: () => <ErrorState title="No se pudo cargar el arma." />,
