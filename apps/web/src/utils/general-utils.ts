a2558e8ed7170081aeb4e864052882e5bae89cee
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-24 11:29:44 -0500

    CHORE: minor changes ⚙️

--- apps/web/src/utils/general-utils.ts
+++ apps/web/src/utils/general-utils.ts
@@ -6,7 +6,12 @@ import {
   SELECT_ECHO_SET,
 } from "@wuwa-mains/constants";
 
-const UNDEFINED_OPTS = {
+const UNDEFINED_GENERAL_OPTS = {
+  label: "No definido",
+  value: "undefined",
+};
+
+const UNDEFINED_ECHOES_OPTS = {
   label: "No definido",
   value: "undefined",
   description: ["No definido"],
@@ -16,7 +21,7 @@ export function getWeaponType(weaponType: string) {
   const WEAPON_TYPE = SELECT_WEAPON_TYPE.find(
     (type) => type.value === weaponType,
   );
-  if (!WEAPON_TYPE) return UNDEFINED_OPTS;
+  if (!WEAPON_TYPE) return UNDEFINED_GENERAL_OPTS;
   return WEAPON_TYPE;
 }
 
@@ -24,7 +29,7 @@ export function getElementTypeName(elementType: string) {
   const ELEMENT_TYPE = SELECT_ELEMENT_TYPE.find(
     (type) => type.value === elementType,
   );
-  if (!ELEMENT_TYPE) return UNDEFINED_OPTS;
+  if (!ELEMENT_TYPE) return UNDEFINED_GENERAL_OPTS;
   return ELEMENT_TYPE;
 }
 
@@ -32,7 +37,7 @@ export function getMainStat(mainStat: string) {
   const MAIN_STAT = SELECT_WEAPON_MAIN_STATS.find(
     (stat) => stat.value === mainStat,
   );
-  if (!MAIN_STAT) return UNDEFINED_OPTS;
+  if (!MAIN_STAT) return UNDEFINED_GENERAL_OPTS;
   return MAIN_STAT;
 }
 
@@ -40,12 +45,12 @@ export function getMaterialType(materialType: string) {
   const MATERIAL_TYPE = SELECT_MATERIAL_TYPE.find(
     (type) => type.value === materialType,
   );
-  if (!MATERIAL_TYPE) return UNDEFINED_OPTS;
+  if (!MATERIAL_TYPE) return UNDEFINED_GENERAL_OPTS;
   return MATERIAL_TYPE;
 }
 
 export function getEchoSet(echoSet: string) {
   const ECHO_SET = SELECT_ECHO_SET.find((set) => set.value === echoSet);
-  if (!ECHO_SET) return UNDEFINED_OPTS;
+  if (!ECHO_SET) return UNDEFINED_ECHOES_OPTS;
   return ECHO_SET;
 }
