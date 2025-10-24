ac7a355fc4625696f64827ecec7af0cae87ef376
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-24 11:19:05 -0500

    FEAT: add echoes router ⚒️

---
+++ packages/api/src/routers/echoes.ts
@@ -0,0 +1,160 @@
+import { TRPCError, type TRPCRouterRecord } from "@trpc/server";
+import { protectedProcedure, publicProcedure } from "../index";
+import { idZodSchema } from "@wuwa-mains/schemas";
+import { eq } from "drizzle-orm";
+import { echoes, echoesSet } from "@wuwa-mains/db/schema/echo-schema";
+import { echoZodSchema } from "@wuwa-mains/schemas/zod/echo-schema";
+import { echoSetsTransformOpts } from "../helpers/option-transform";
+
+export const echoesRouter = {
+  get: publicProcedure.query(async ({ ctx }) => {
+    try {
+      const echoes = await ctx.db.query.echoes.findMany({
+        with: { sets: true },
+      });
+      return echoes;
+    } catch (error) {
+      console.error("Error getting echoes:", error);
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Ha ocurrido un error al obtener los ecos.",
+        cause: error,
+      });
+    }
+  }),
+  unique: publicProcedure.input(idZodSchema).query(async ({ ctx, input }) => {
+    try {
+      const { id } = input;
+
+      const echo = await ctx.db.query.echoes.findFirst({
+        where: eq(echoes.id, id),
+        with: { sets: true },
+      });
+
+      if (!echo) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Eco no encontrado.",
+        });
+      }
+
+      return echo;
+    } catch (error) {
+      console.error("Error getting echo:", error);
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Ha ocurrido un error al obtener el eco.",
+        cause: error,
+      });
+    }
+  }),
+  update: protectedProcedure
+    .input(echoZodSchema.extend(idZodSchema.shape))
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const { sets, id, ...echoData } = input;
+
+        const result = await ctx.db.transaction(async (tx) => {
+          const [updated] = await tx
+            .update(echoes)
+            .set(echoData)
+            .where(eq(echoes.id, id))
+            .returning();
+
+          if (!updated) {
+            throw new TRPCError({
+              code: "NOT_FOUND",
+              message: "Eco no encontrado",
+            });
+          }
+
+          await tx.delete(echoesSet).where(eq(echoesSet.id, id));
+
+          if (sets?.length) {
+            const newStyles = echoSetsTransformOpts({
+              echoSets: sets,
+              echoId: id,
+            });
+            await tx.insert(echoesSet).values(newStyles);
+          }
+
+          return updated;
+        });
+
+        return {
+          code: "SUCCESS",
+          message: "Eco actualizado correctamente.",
+          data: result,
+        };
+      } catch (error) {
+        console.error("Error updating echo:", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Ha ocurrido un error al actualizar el eco.",
+          cause: error,
+        });
+      }
+    }),
+  add: protectedProcedure
+    .input(echoZodSchema)
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const { sets, ...echoData } = input;
+
+        const result = await ctx.db.transaction(async (tx) => {
+          const [added] = await tx.insert(echoes).values(echoData).returning();
+
+          if (!added) {
+            throw new TRPCError({
+              code: "INTERNAL_SERVER_ERROR",
+              message: "Error al crear el eco.",
+            });
+          }
+
+          if (sets.length > 0) {
+            const newStyles = echoSetsTransformOpts({
+              echoSets: sets,
+              echoId: added.id,
+            });
+            await tx.insert(echoesSet).values(newStyles);
+          }
+
+          return added;
+        });
+
+        return {
+          code: "SUCCESS",
+          message: "Eco creado con éxito!",
+          data: result,
+        };
+      } catch (error) {
+        console.error("Error creating echo:", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Error al crear el eco.",
+          cause: error,
+        });
+      }
+    }),
+  delete: protectedProcedure
+    .input(idZodSchema)
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const { id } = input;
+
+        await ctx.db.delete(echoes).where(eq(echoes.id, id));
+
+        return {
+          code: "SUCCESS",
+          message: "Eco eliminado.",
+        };
+      } catch (error) {
+        console.error("Error deleting echo:", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Error al eliminar el eco.",
+          cause: error,
+        });
+      }
+    }),
+} satisfies TRPCRouterRecord;
