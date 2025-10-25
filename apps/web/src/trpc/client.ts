5dc0c94f496e11dbe3521367b5d75b77795fcacd
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-25 10:09:42 -0500

    FEAT: add stale time for the queries ⚙️

--- apps/web/src/trpc/client.ts
+++ apps/web/src/trpc/client.ts
@@ -20,6 +20,7 @@ export const queryClient = new QueryClient({
       });
     },
   }),
+  defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } },
 });
 
 export const trpcClient = createTRPCClient<AppRouter>({
