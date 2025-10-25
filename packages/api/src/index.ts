dde2d327148a63d7b9422d32ad40fd0791803a90
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-25 14:20:04 -0500

    FEAT: add middlewares by role ⚙️

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -10,9 +10,8 @@ export const t = initTRPC.context<Context>().create({
 
 export const router = t.router;
 
-export const publicProcedure = t.procedure;
-
-export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+// Middleware Procedures
+export const isAuthed = t.middleware(({ ctx, next }) => {
   if (!ctx.session) {
     throw new TRPCError({
       code: "UNAUTHORIZED",
@@ -22,3 +21,36 @@ export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
   }
   return next({ ctx: { ...ctx, session: ctx.session } });
 });
+
+export const isAdmin = t.middleware(({ ctx, next }) => {
+  const currentRole = ctx.session?.user.role;
+  if (currentRole !== "ADMIN") {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You are not authorized to perform this action",
+      cause: "Insufficient permissions (Only Admin)",
+    });
+  }
+  return next({ ctx: { ...ctx, session: ctx.session } });
+});
+
+export const isEditor = t.middleware(({ ctx, next }) => {
+  const currentRole = ctx.session?.user.role;
+  if (currentRole !== "EDITOR" && currentRole !== "ADMIN") {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You are not authorized to perform this action",
+      cause: "Insufficient permissions (Only Editor or Admin)",
+    });
+  }
+  return next({ ctx: { ...ctx, session: ctx.session } });
+});
+
+// TRPC Procedures
+export const publicProcedure = t.procedure;
+
+export const editorProcedure = t.procedure.use(isEditor);
+
+export const adminProcedure = t.procedure.use(isAdmin);
+
+export const protectedProcedure = t.procedure.use(isAuthed);
