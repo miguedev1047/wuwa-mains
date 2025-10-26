116011c96d74ce2a9e7c2345ea4fcd0e3f561127
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-25 20:03:25 -0500

    CHORE: add head content for the resonator route ⚒️

--- apps/web/src/routes/_protected/panel/resonators/$id/index.tsx
+++ apps/web/src/routes/_protected/panel/resonators/$id/index.tsx
@@ -7,6 +7,22 @@ import { LoaderState } from "@/components/state-ui/loader";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_protected/panel/resonators/$id/")({
+  loader: async ({ params, context }) => {
+    const data = await context.queryClient.ensureQueryData(
+      context.trpc.resonators.unique.queryOptions({ id: params.id }),
+    );
+    return { data };
+  },
+  head: async ({ loaderData }) => {
+    const resonatorName = loaderData?.data.name;
+    return {
+      meta: [
+        {
+          title: `Wuwa Mains - ${resonatorName}`,
+        },
+      ],
+    };
+  },
   pendingComponent: () => <LoaderState title="Cargando resonador..." />,
   errorComponent: () => <ErrorState title="No se pudo cargar el resonador." />,
   component: RouteComponent,
