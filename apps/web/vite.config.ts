40389bcd72a8a94e07340fbccd95bec4edb481c7
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-25 20:07:41 -0500

    FEAT: add nitro plugin ⚒️

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,11 +3,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    nitroV2Plugin(),
     tsconfigPaths(),
     tailwindcss(),
     tanstackStart(),
