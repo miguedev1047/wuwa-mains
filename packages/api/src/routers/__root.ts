707c5500e1d7957e702190866cbe1e1163cbfb95
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-26 01:58:51 -0500

    FEAT: add skills router ⚒️

--- packages/api/src/routers/__root.ts
+++ packages/api/src/routers/__root.ts
@@ -1,21 +1,14 @@
-import { protectedProcedure, publicProcedure, router } from "../index";
+import { router } from "../index";
 import { echoesRouter } from "./echoes";
 import { materialsRouter } from "./materials";
 import { resonatorsRouter } from "./resonators";
+import { skillsRouter } from "./skills";
 import { weaponsRouter } from "./weapons";
 
 export const appRouter = router({
-  healthCheck: publicProcedure.query(() => {
-    return "OK";
-  }),
-  privateData: protectedProcedure.query(({ ctx }) => {
-    return {
-      message: "This is private",
-      user: ctx.session.user,
-    };
-  }),
   resonators: resonatorsRouter,
   weapons: weaponsRouter,
+  skills: skillsRouter,
   materials: materialsRouter,
   echoes: echoesRouter,
 });
