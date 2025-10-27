5b70e47db65351eeedf299bd544b027f56259141
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-27 11:09:00 -0500

    FEAT: add export all skill section components ⚒️

---
+++ apps/web/src/routes/_protected/panel/resonators/$id/-sections/resonator-skills/-components/index.ts
@@ -0,0 +1,5 @@
+export * from "./resonator-skill-list";
+export * from "./resonator-skill-item";
+export * from "./resonator-skill-form";
+export * from "./resonnator-skill-delete";
+export * from "./resonator-skill-empty";
