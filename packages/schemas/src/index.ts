2ddd6011b23c786e3bc0d8942a5460a2d345338c
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-27 10:39:38 -0500

    CHORE: remove skill schema ⚙️

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -3,4 +3,3 @@ export * from "./zod/option-schema";
 export * from "./zod/resonator-schema";
 export * from "./zod/weapon-schema";
 export * from "./zod/echo-schema";
-export * from "./zod/skill-schema";
