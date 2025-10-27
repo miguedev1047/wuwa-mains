a5878552b5ab74cf8b43fa8b65a3dcd345f1d459
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-27 11:06:22 -0500

    FEAT: add resonator skills types ⚒️

--- apps/web/src/routes/_protected/panel/resonators/-types/index.ts
+++ apps/web/src/routes/_protected/panel/resonators/-types/index.ts
@@ -2,12 +2,17 @@ import {
   combatStyles,
   resonators,
 } from "@wuwa-mains/db/schema/resonator-schema";
-import { type ResonatorZodSchema } from "@wuwa-mains/schemas/zod/resonator-schema";
-import { type InferSelectModel } from "drizzle-orm";
+import type { ResonatorZodSchema } from "@wuwa-mains/schemas/zod/resonator-schema";
+import type { ResonatorSkillZodSchema } from "@wuwa-mains/schemas/zod/resonator-schema";
+import type { InferSelectModel } from "drizzle-orm";
 
 export interface ResonatorFormProps {
   data?: ResonatorZodSchema;
   resonatorId?: string;
+}
+
+export interface ResonatorSkillFormProps {
+  data?: ResonatorSkillZodSchema;
 }
 
 export type ResonatorDatabaseSchema = InferSelectModel<typeof resonators> & {
