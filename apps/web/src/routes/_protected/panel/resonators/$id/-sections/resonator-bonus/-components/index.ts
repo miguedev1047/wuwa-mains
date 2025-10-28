376ecf51df4f153ac77567e585f9c7ed0505047d
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-28 12:40:09 -0500

    FEAT: export all bonus components ⚒️

---
+++ apps/web/src/routes/_protected/panel/resonators/$id/-sections/resonator-bonus/-components/index.ts
@@ -0,0 +1,5 @@
+export * from "./resonator-bonus-list";
+export * from "./resonator-bonus-empty";
+export * from "./resonator-bonus-form";
+export * from "./resonator-bonus-item";
+export * from "./resonator-bonus-delete";
