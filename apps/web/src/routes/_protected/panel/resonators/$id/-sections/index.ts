cd0c5e7568d009100d60817cd489073b17382042
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-10-31 14:33:18 -0500

    FEAT: add resonator levels section 🎨

--- apps/web/src/routes/_protected/panel/resonators/$id/-sections/index.ts
+++ apps/web/src/routes/_protected/panel/resonators/$id/-sections/index.ts
@@ -2,3 +2,4 @@ export * from "./resonator-skills";
 export * from "./resonator-info";
 export * from "./resonator-bonus";
 export * from "./resonator-chain-resonance";
+export * from "./resonator-levels";
