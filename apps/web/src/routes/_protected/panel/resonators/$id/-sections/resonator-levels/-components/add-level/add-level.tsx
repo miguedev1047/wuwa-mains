a393049e2b8242ebaa2bd1afbe3dd923be409494
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-11-01 04:44:08 -0500

    CHORE: rollback to monorepo structure ⚙️

---
+++ apps/web/src/routes/_protected/panel/resonators/$id/-sections/resonator-levels/-components/add-level/add-level.tsx
@@ -0,0 +1,86 @@
+import { useAddLevel } from "@/routes/_protected/panel/resonators/$id/-sections/resonator-levels/-hooks";
+import { SELECT_LEVELS } from "@wuwa-mains/constants";
+import { PlusIcon } from "lucide-react";
+
+export function AddLevel() {
+  const { dialogOpen, form, formId, isPending, onSubmit, setDialogOpen } =
+    useAddLevel();
+
+  return (
+    <form.AppForm>
+      <form.FormSheetHandle id={formId} onSubmit={onSubmit}>
+        <form.FormSheetRoot
+          open={dialogOpen}
+          onOpenChange={setDialogOpen}
+          renderHeader={
+            <form.FormSheetHeader>
+              <form.FormSheetTitle>Nuevo nivel</form.FormSheetTitle>
+              <form.FormSheetDescription>
+                Agrega un nuevo nivel al resonador.
+              </form.FormSheetDescription>
+            </form.FormSheetHeader>
+          }
+          renderContent={
+            <form.FormSheetContent>
+              <form.AppField name="level">
+                {(field) => (
+                  <field.SelectField
+                    label="Selecciona un nivel"
+                    description="Seleccciona un nivel."
+                    placeholder="Nivel 90"
+                    items={SELECT_LEVELS}
+                    showError
+                  />
+                )}
+              </form.AppField>
+              <form.AppField name="hp">
+                {(field) => (
+                  <field.NumberField
+                    label="Vida"
+                    description="Ingresa la vida del resonador."
+                    placeholder="0"
+                    showError
+                  />
+                )}
+              </form.AppField>
+              <form.AppField name="atq">
+                {(field) => (
+                  <field.NumberField
+                    label="Ataque"
+                    description="Ingresa el ataque del resonador."
+                    placeholder="0"
+                    showError
+                  />
+                )}
+              </form.AppField>
+              <form.AppField name="def">
+                {(field) => (
+                  <field.NumberField
+                    label="Defensa"
+                    description="Ingresa la defensa del resonador."
+                    placeholder="0"
+                    showError
+                  />
+                )}
+              </form.AppField>
+            </form.FormSheetContent>
+          }
+          renderFooter={
+            <form.FormSheetFooter>
+              <form.AppForm>
+                <form.SubmitButton
+                  isPending={isPending}
+                  type="submit"
+                  form={formId}
+                >
+                  <PlusIcon />
+                  Agregar nivel
+                </form.SubmitButton>
+              </form.AppForm>
+            </form.FormSheetFooter>
+          }
+        />
+      </form.FormSheetHandle>
+    </form.AppForm>
+  );
+}
---
+++ apps/web/src/routes/_protected/panel/resonators/$id/-sections/resonator-levels/-hooks/use-delete-level.ts
@@ -0,0 +1,47 @@
+import type { LevelZodSchema } from "@wuwa-mains/schemas/zod/resonator-schema";
+import { useTRPC } from "@/trpc/root";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useParams } from "@tanstack/react-router";
+import { toast } from "sonner";
+
+export function useDeleteLevel(data: LevelZodSchema) {
+  const { id: resonatorId } = useParams({
+    from: "/_protected/panel/resonators/$id/",
+  });
+
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  const queryKey = trpc.resonators.unique.queryKey({
+    id: resonatorId,
+  });
+  const invalidateQuery = () => {
+    queryClient.invalidateQueries({ queryKey: queryKey });
+  };
+
+  const deleteMutationOpts = trpc.levels.delete.mutationOptions({
+    onSuccess: (ctx) => {
+      const { message } = ctx;
+      toast.success(message);
+    },
+    onError: (ctx) => {
+      const { message } = ctx;
+      toast.error(message);
+    },
+    onSettled: () => {
+      invalidateQuery();
+    },
+  });
+  const deleteMutation = useMutation(deleteMutationOpts);
+
+  const isPending = deleteMutation.isPending;
+
+  const onDelete = () => {
+    if (!data.id) {
+      return toast.error("Nivel no encontrado.");
+    }
+    deleteMutation.mutate({ id: data.id });
+  };
+
+  return { isPending, onDelete };
+}
