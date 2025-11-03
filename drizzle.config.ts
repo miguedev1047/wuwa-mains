3a26b198b1d0bd57b0371b2616d849c6362d1c97
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-11-03 17:10:59 -0500

    CHORE: update out and schema folders ⚒️

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,13 @@
-import { config } from 'dotenv'
-import { defineConfig } from 'drizzle-kit'
+import "dotenv/config";
 
-config()
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: './src/server/db/migrations',
-  schema: './src/server/db/schemas',
-  dialect: 'turso',
+  out: "./src/services/db/migrations",
+  schema: "./src/services/db/schema",
+  dialect: "turso",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "",
-    authToken: process.env.DATABASE_AUTH_TOKEN,
+    url: process.env.DATABASE_URL as string,
+    authToken: process.env.DATABASE_AUTH_TOKEN as string,
   },
-})
+});
