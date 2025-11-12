3af4631011099f5a31f4489b55d778a5b757f0bb
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-11-12 03:03:59 -0500

    FEAT: add nitro vite plugin ⚙️

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   plugins: [
     viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact({
       babel: { plugins: ["babel-plugin-react-compiler"] },
     }),
